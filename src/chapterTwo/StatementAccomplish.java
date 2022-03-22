68379a2ad04c528a7f59c2c50356f92ea0c81816
Ruth Adeyemi Adebimpe <94833329+ruthadeyemi@users.noreply.github.com>
2022-03-22 14:48:36 +0100

    StatementAccomplish

--- src/chapterTwo/StatementAccomplish.java
+++ src/chapterTwo/StatementAccomplish.java
@@ -1,9 +1,10 @@
-package chapterTwo;
-//2.8
+package chapterTwo;//2.8
 public class StatementAccomplish {
     public static void main(String[] args) {
-      // System.out.println("Enter an integer");
-      //  int a = b*c;
+       System.out.println("Enter an integer");
+        int b = 0;
+        int c = 0;
+        int a = b*c;
        // a program performs a sample payroll calculation.
     }
 }
